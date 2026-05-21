6da64ca7d46b5be1dc419b9304a3bdde4c568d86
OscarOrtega <ortegaoscar14@gmail.com>
2026-05-21 11:08:09 -0500

    feat: add procedural Astronaut with Float

---
+++ src/three/objects/Astronaut.tsx
@@ -0,0 +1,92 @@
+import { Float } from '@react-three/drei';
+
+// Replace the contents of <AstronautModel /> with `useGLTF('/models/astronaut.glb')`
+// in a later phase when a CC0/CC-BY astronaut model is added to public/models/.
+function AstronautModel() {
+  return (
+    <group>
+      {/* Helmet */}
+      <mesh position={[0, 1.05, 0]}>
+        <sphereGeometry args={[0.42, 32, 32]} />
+        <meshStandardMaterial
+          color="#0a1224"
+          metalness={0.85}
+          roughness={0.15}
+          emissive="#00d4ff"
+          emissiveIntensity={0.15}
+        />
+      </mesh>
+
+      {/* Visor */}
+      <mesh position={[0, 1.08, 0.18]}>
+        <sphereGeometry args={[0.32, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
+        <meshStandardMaterial
+          color="#00d4ff"
+          metalness={1}
+          roughness={0.05}
+          emissive="#00d4ff"
+          emissiveIntensity={0.4}
+        />
+      </mesh>
+
+      {/* Body */}
+      <mesh position={[0, 0.1, 0]}>
+        <capsuleGeometry args={[0.36, 0.8, 8, 16]} />
+        <meshStandardMaterial color="#e0fbff" metalness={0.2} roughness={0.6} />
+      </mesh>
+
+      {/* Backpack */}
+      <mesh position={[0, 0.2, -0.32]}>
+        <boxGeometry args={[0.6, 0.7, 0.25]} />
+        <meshStandardMaterial color="#67e8f9" metalness={0.4} roughness={0.4} />
+      </mesh>
+
+      {/* Backpack lights */}
+      <mesh position={[-0.18, 0.45, -0.46]}>
+        <sphereGeometry args={[0.04, 16, 16]} />
+        <meshStandardMaterial color="#00d4ff" emissive="#00d4ff" emissiveIntensity={3} />
+      </mesh>
+      <mesh position={[0.18, 0.45, -0.46]}>
+        <sphereGeometry args={[0.04, 16, 16]} />
+        <meshStandardMaterial color="#00d4ff" emissive="#00d4ff" emissiveIntensity={3} />
+      </mesh>
+
+      {/* Left arm */}
+      <mesh position={[-0.5, 0.2, 0]} rotation={[0, 0, Math.PI / 12]}>
+        <capsuleGeometry args={[0.12, 0.7, 8, 16]} />
+        <meshStandardMaterial color="#e0fbff" metalness={0.2} roughness={0.6} />
+      </mesh>
+
+      {/* Right arm */}
+      <mesh position={[0.5, 0.2, 0]} rotation={[0, 0, -Math.PI / 12]}>
+        <capsuleGeometry args={[0.12, 0.7, 8, 16]} />
+        <meshStandardMaterial color="#e0fbff" metalness={0.2} roughness={0.6} />
+      </mesh>
+
+      {/* Left leg */}
+      <mesh position={[-0.2, -0.85, 0]}>
+        <capsuleGeometry args={[0.14, 0.6, 8, 16]} />
+        <meshStandardMaterial color="#e0fbff" metalness={0.2} roughness={0.6} />
+      </mesh>
+
+      {/* Right leg */}
+      <mesh position={[0.2, -0.85, 0]}>
+        <capsuleGeometry args={[0.14, 0.6, 8, 16]} />
+        <meshStandardMaterial color="#e0fbff" metalness={0.2} roughness={0.6} />
+      </mesh>
+    </group>
+  );
+}
+
+export default function Astronaut() {
+  return (
+    <Float
+      speed={1.2}
+      rotationIntensity={0.3}
+      floatIntensity={0.6}
+      position={[0, 0.5, 0]}
+    >
+      <AstronautModel />
+    </Float>
+  );
+}
