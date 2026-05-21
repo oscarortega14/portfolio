57f280de5c388c54ef97d739c150d7fcbdc3165e
OscarOrtega <ortegaoscar14@gmail.com>
2026-05-21 14:59:31 -0500

    feat: add introState machine to scrollStore

--- src/stores/scrollStore.ts
+++ src/stores/scrollStore.ts
@@ -1,15 +1,26 @@
 import { create } from 'zustand';
 
+export type IntroState = 'loading' | 'intro' | 'ready';
+
 type ScrollState = {
   progress: number;
   devCameraMode: boolean;
+  introState: IntroState;
+  introStartedAt: number | null;
   setProgress: (p: number) => void;
   toggleDevCameraMode: () => void;
+  startIntro: () => void;
+  finishIntro: () => void;
 };
 
 export const useScrollStore = create<ScrollState>((set) => ({
   progress: 0,
   devCameraMode: false,
+  introState: 'loading',
+  introStartedAt: null,
   setProgress: (progress) => set({ progress }),
   toggleDevCameraMode: () => set((s) => ({ devCameraMode: !s.devCameraMode })),
+  startIntro: () =>
+    set({ introState: 'intro', introStartedAt: performance.now() }),
+  finishIntro: () => set({ introState: 'ready' }),
 }));
